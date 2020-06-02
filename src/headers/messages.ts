17ad5bb5ef680a0b792a1352011b8df488ab4983
David Barrat <david@barrat.io>
2020-06-02 19:41:56 +0200

    code cleanup

---
+++ src/headers/messages.ts
@@ -0,0 +1,4 @@
+export const NON_AUTHORIZED_HEADER = 'Non-authorized header'
+export const MISSING_HEADER = 'Missing header'
+export const INVALID_HEADER = 'Invalid header'
+export const VALID_HEADER = 'Valid header'
--- src/headers/utils.ts
+++ src/headers/utils.ts
@@ -2,45 +2,52 @@ import {
     HeaderSecurityCheck
 } from './index.d'
 
+import {
+    NON_AUTHORIZED_HEADER,
+    VALID_HEADER,
+    INVALID_HEADER,
+    MISSING_HEADER
+} from './messages'
+
 export const validateHeaders = (headers: any, rules: HeaderSecurityCheck[]) => {
     const errors = []
     const passed = []
 
     rules.map((el: HeaderSecurityCheck) => {
-        const name = el.name
+        const name = el?.name
         const header = headers[name]
         let header_meta = {
             name, header
         }
 
-        if (header && el.reject === true) {
+        if (header && el?.reject === true) {
             errors.push({
                 ...header_meta,
-                message: 'Non-authorized header'
+                message: NON_AUTHORIZED_HEADER
             })
         }
-        else if (!header && el.required === true) {
+        else if (!header && el?.required === true) {
             errors.push({
                 ...header_meta,
-                message: 'Missing header'
+                message: MISSING_HEADER
             })
         }
-        else if (header && el.required === true && el.possible.length === 0) {
+        else if (header && el?.required === true && el?.possible.length > 0 && !el?.possible.includes(header)) {
+            errors.push({
+                ...header_meta,
+                message: INVALID_HEADER,
+            })
+        }
+        else if (header && el?.required === true && el?.possible.length === 0) {
             passed.push({
                 ...header_meta,
-                message: 'Valid header'
+                message: VALID_HEADER
             })
         }
-        else if (header && el.required === true && el.possible.length > 0 && !el.possible.includes(header)) {
-            errors.push({
-                ...header_meta,
-                message: 'Invalid header',
-            })
-        }
-        else if (header && el.required === true && el.possible.length > 0 && el.possible.includes(header)) {
+        else if (header && el?.required === true && el?.possible.length > 0 && el?.possible.includes(header)) {
             passed.push({
                 ...header_meta,
-                message: 'Valid header'
+                message: VALID_HEADER
             })
         }
     })
